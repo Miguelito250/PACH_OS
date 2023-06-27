4fc3f08f661fbc6e6c74403a21a8b2a53c0d8e29
camiloolea20077 <camilo.olea@est.iudigital.edu.co>
2023-06-26 20:04:40 -0500

    Proveedores 3

---
+++ Pach_OS/Controllers/DetalleVentasController.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Pach_OS.Models;
+
+namespace Pach_OS.Controllers
+{
+    public class DetalleVentasController : Controller
+    {
+        private readonly Pach_OSContext _context;
+
+        public DetalleVentasController(Pach_OSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DetalleVentas
+        public async Task<IActionResult> Index()
+        {
+            var pach_OSContext = _context.DetalleVentas.Include(d => d.Productos).Include(d => d.Venta);
+            return View(await pach_OSContext.ToListAsync());
+        }
+
+        // GET: DetalleVentas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.DetalleVentas == null)
+            {
+                return NotFound();
+            }
+
+            var detalleVenta = await _context.DetalleVentas
+                .Include(d => d.Productos)
+                .Include(d => d.Venta)
+                .FirstOrDefaultAsync(m => m.IdDetalleVenta == id);
+            if (detalleVenta == null)
+            {
+                return NotFound();
+            }
+
+            return View(detalleVenta);
+        }
+
+        //GET: DetalleVentas/Create
+        public IActionResult Create(int ventaId)
+        {
+            ViewBag.idVentaDet = ventaId;
+            ViewData["ProductosId"] = new SelectList(_context.Productos, "IdProductos", "NomProducto");
+            var detalleVenta = _context.DetalleVentas.Where(d => d.VentaId == ventaId).ToList();
+            ViewBag.detalleVenta = detalleVenta;
+            ViewData["VentaId"] = new SelectList(_context.Ventas, "IdVentas", "IdVentas");
+            return View(new DetalleVenta { VentaId = ventaId }); // Pasar el objeto DetalleVenta con el valor de ventaId asignado
+        }
+
+        // POST: DetalleVentas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DetalleVenta detalleVenta)
+        {
+            if (ModelState.IsValid)
+            {
+                var producto = _context.Productos.FirstOrDefault(p => p.IdProductos == detalleVenta.ProductosId);
+
+                if (producto != null)
+                {
+                    var receta = _context.ProductosInsumos.Where(pi => pi.ProductosId == producto.IdProductos).ToList();
+
+                    foreach (var insumoReceta in receta)
+                    {
+                        var insumo = _context.Insumos.FirstOrDefault(i => i.IdInsumos == insumoReceta.InsumosId);
+                        var cantidadInsumo = insumo != null ? insumo.CantInsumo : 0;
+                        var cantidadNecesaria = insumoReceta.CantInsumo * detalleVenta.CantVendida;
+
+                        if (cantidadNecesaria > cantidadInsumo)
+                        {
+                            ModelState.AddModelError("", $"No hay suficientes insumos ({insumo?.NomInsumo}) disponibles para este producto.");
+                            return RedirectToAction("Create", "DetalleVentas", new { ventaId = detalleVenta.VentaId });
+                        }
+
+                        // Actualizar la cantidad de insumo en la tabla Insumos
+                        insumo.CantInsumo -= cantidadNecesaria;
+                        _context.Update(insumo);
+                    }
+
+                    _context.Add(detalleVenta);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("Create", "DetalleVentas", new { ventaId = detalleVenta.VentaId });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "El producto seleccionado no existe.");
+                }
+            }
+
+            ViewData["ProductosId"] = new SelectList(_context.Productos, "IdProductos", "NomProducto", detalleVenta.ProductosId);
+            ViewData["VentaId"] = new SelectList(_context.Ventas, "IdVentas", "IdVentas", detalleVenta.VentaId);
+
+            return RedirectToAction("Create", "DetalleVentas", new { ventaId = detalleVenta.VentaId });
+        }
+
+
+        // GET: DetalleVentas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.DetalleVentas == null)
+            {
+                return NotFound();
+            }
+
+            var detalleVenta = await _context.DetalleVentas.FindAsync(id);
+            if (detalleVenta == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductosId"] = new SelectList(_context.Productos, "IdProductos", "IdProductos", detalleVenta.ProductosId);
+            ViewData["VentaId"] = new SelectList(_context.Ventas, "IdVentas", "IdVentas", detalleVenta.VentaId);
+            return View(detalleVenta);
+        }
+
+        // GET: DetalleVentas/ConfirmarVenta/5
+        public async Task<IActionResult> ConfirmarVenta(int? id, DetalleVenta detalleVenta)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas.FindAsync(id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            // Cargar los detalles de venta incluyendo la propiedad de navegación "Productos"
+            var detallesVenta = await _context.DetalleVentas
+                .Include(d => d.Productos)
+                .Where(d => d.VentaId == id)
+                .ToListAsync();
+
+            // Calcula el total de la venta sumando los precios totales de los detalles de venta
+            int totalVenta = (int)detallesVenta.Sum(d => d.Productos.PrecioVenta * d.CantVendida);
+
+            // Obtén el monto del pago a domicilio y verifica si es nulo
+            int pagoDomicilio = venta.PagoDomicilio ?? 0;
+
+            // Agrega el monto del pago a domicilio al total de la venta
+            int totalConDomicilio = totalVenta + pagoDomicilio;
+
+            // Asigna el total de la venta a la propiedad TotalVenta del modelo Venta
+            venta.TotalVenta = totalConDomicilio;
+
+            // Guarda los cambios en la base de datos
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Edit", "Ventas", new { id = id });
+        }
+
+
+
+
+        // POST: DetalleVentas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdDetalleVenta,VentaId,CantVendida,ProductosId, Precio, TotalPrecio")] DetalleVenta detalleVenta)
+        {
+            if (id != detalleVenta.IdDetalleVenta)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(detalleVenta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DetalleVentaExists(detalleVenta.IdDetalleVenta))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductosId"] = new SelectList(_context.Productos, "IdProductos", "IdProductos", detalleVenta.ProductosId);
+            ViewData["VentaId"] = new SelectList(_context.Ventas, "IdVentas", "IdVentas", detalleVenta.VentaId);
+            return View(detalleVenta);
+        }
+
+        // GET: DetalleVentas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.DetalleVentas == null)
+            {
+                return NotFound();
+            }
+
+            var detalleVenta = await _context.DetalleVentas
+                .Include(d => d.Productos)
+                .Include(d => d.Venta)
+                .FirstOrDefaultAsync(m => m.IdDetalleVenta == id);
+            if (detalleVenta == null)
+            {
+                return NotFound();
+            }
+
+            return View(detalleVenta);
+        }
+
+        // POST: DetalleVentas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.DetalleVentas == null)
+            {
+                return Problem("Entity set 'Pach_OSContext.DetalleVentas'  is null.");
+            }
+            var detalleVenta = await _context.DetalleVentas.FindAsync(id);
+            if (detalleVenta != null)
+            {
+                _context.DetalleVentas.Remove(detalleVenta);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Create", "DetalleVentas", new { ventaId = detalleVenta.VentaId });
+        }
+
+        private bool DetalleVentaExists(int id)
+        {
+            return (_context.DetalleVentas?.Any(e => e.IdDetalleVenta == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Pach_OS/Controllers/ProductoesController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Pach_OS.Models;
+
+namespace Pach_OS.Controllers
+{
+    public class ProductoesController : Controller
+    {
+        private readonly Pach_OSContext _context;
+
+        public ProductoesController(Pach_OSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Productoes
+        public async Task<IActionResult> Index()
+        {
+            var pach_OSContext = _context.Productos.Include(p => p.Categoria);
+            return View(await pach_OSContext.ToListAsync());
+        }
+
+        // GET: Productoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.IdProductos == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Productoes/Create
+        public IActionResult Create()
+        {
+            ViewBag.Categorias = _context.Categorias.Select(c => new SelectListItem
+            {
+                Value = c.IdCategoria.ToString(),
+                Text = c.NomCategoria
+            }).ToList();
+            return View();
+        }
+
+        // POST: Productoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProductos,NomProducto,PrecioVenta,Estado,CategoriaId,TamanoPizza,MaximoSabores")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Create", "ProductosInsumoes", new { id= producto.IdProductos });
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "IdCategoria", "IdCategoria", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // GET: Productoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "IdCategoria", "IdCategoria", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // POST: Productoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProductos,NomProducto,PrecioVenta,Estado,CategoriaId,TamanoPizza,MaximoSabores")] Producto producto)
+        {
+            if (id != producto.IdProductos)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.IdProductos))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "IdCategoria", "IdCategoria", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // GET: Productoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.IdProductos == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: Productoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Productos == null)
+            {
+                return Problem("Entity set 'Pach_OSContext.Productos'  is null.");
+            }
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto != null)
+            {
+                var productosInsumoList = _context.ProductosInsumos.Where(d => d.ProductosId == id).ToList();
+                foreach (var i in productosInsumoList) { 
+                    if(i.ProductosId == id)
+                    {
+                        _context.ProductosInsumos.Remove(i);
+                    }
+                }
+                _context.Productos.Remove(producto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductoExists(int id)
+        {
+          return (_context.Productos?.Any(e => e.IdProductos == id)).GetValueOrDefault();
+        }
+    }
+}
