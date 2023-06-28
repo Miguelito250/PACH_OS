7e5e0befcb665d79e1a8a0cb84df8e1f38f4ea49
CaterinMB <cmartinez2131@misena.edu.co>
2023-06-27 22:44:58 -0500

    login

--- Pach_OS/Models/Pach_OSContext.cs
+++ Pach_OS/Models/Pach_OSContext.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Pach_OS.Models
 {
-    public partial class Pach_OSContext : DbContext
+    public partial class Pach_OSContext : IdentityDbContext
     {
         public Pach_OSContext()
         {
@@ -404,7 +405,7 @@ namespace Pach_OS.Models
                     .HasForeignKey(d => d.UsuarioId)
 
                     .HasConstraintName("FK__ventas__usuario___7E37BEF6");
-
+                base.OnModelCreating(modelBuilder);
             });
 
             OnModelCreatingPartial(modelBuilder);
